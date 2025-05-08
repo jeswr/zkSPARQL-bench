c063494c2356b0dd4a5cb91565808ceabc09062f
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2025-05-08 19:53:52 +0100

    chore: add LUBM queries

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
 mkdir ./dist/
 cp context.json dist/context.json
 
-
+# BSBM Generation
 docker run -v "$PWD:/app/data" -e "DATA_DESTINATION=bsbm" vcity/bsbm generate -s ttl
+wget https://github.com/VCityTeam/BSBM/archive/refs/tags/v1.4.0.zip
+unzip v1.4.0.zip
+rm -rf v1.4.0.zip
+
 node bsbm.js
 
 rm -rf bsbm
